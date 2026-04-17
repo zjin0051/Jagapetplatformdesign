8382b30d0dd406cb89feee77cced90abcf5e062c
May <mhoo0063@student.monash.edu>
2026-04-17 10:47:13 +0800

    feat: compare function tryout version 2
    - compare function tryout version 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
-import { CompareProvider } from "./app/context/CompareContext";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <CompareProvider>
-    <App />
-  </CompareProvider>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
